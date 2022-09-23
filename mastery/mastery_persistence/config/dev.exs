52cd9cd2de70df3baba61ced4b6284bd42d8e272
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-09-22 20:56:47 -0600

    feat: add ecto config

--- mastery/mastery_persistence/config/dev.exs
+++ mastery/mastery_persistence/config/dev.exs
@@ -2,5 +2,7 @@ use Mix.Config
 
 config :mastery_persistence, MasteryPersistence.Repo,
   database: "mastery_dev",
-  hostname: "localhost"
+  hostname: "localhost",
+  username: "postgres",
+  password: "abc123"
 
